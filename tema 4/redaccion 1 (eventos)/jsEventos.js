839ab068b63e900f88b00a16b100974eaed9c94a
isidro lucena santiago <darksirius91@gmail.com>
2016-11-02 10:18:09 +0100

    version 1.1

--- tema 4/redaccion 1 (eventos)/jsEventos.js
+++ tema 4/redaccion 1 (eventos)/jsEventos.js
@@ -1,9 +1,13 @@
 function click(){
 	alert("deja");
 }
-window.onload(){
-	window.body.onclick=click;
-	
-	//addEventListener("click",click);
+function gps(){
+	var px=window.getElementById("posx");
+	var py=window.getElementById("posy");
+	px.innerHTML="posicion x="+window.screenX;
+	py.innerHTML="posicion Y="+window.screenY;
+	window.onload();
 }
+window.getElementById("con").addEventListener("mousemove",gps);
+//window.onclick=click;
 
