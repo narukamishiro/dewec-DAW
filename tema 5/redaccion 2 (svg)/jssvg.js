fae9573c6c44a7514b7314829ace19c01514f6c9
isidro lucena santiago <darksirius91@gmail.com>
2016-12-07 10:20:21 +0100

    umm

--- tema 5/redaccion 2 (svg)/jssvg.js
+++ tema 5/redaccion 2 (svg)/jssvg.js
@@ -9,10 +9,50 @@ class juego(){
 class circle(tab,rad,col,velx,vel){
 	var rad;
 	var col;
-	constructor(tab,rad,col){}
+	ptscol=[];
+	var x0=0;
+	var y0=0;
+	constructor(tab,rad,col){
+		
+	}
 	function draw(){}
-	function hit(){
-		var ptscol=[];
+	function hitbox(){
+		ptscol=[];
+		ang=0;
+		do{
+			var ang2 =tradAng(ang);
+			var x=x0+getX(rad,ang2);
+			var y=y0+getY(rad,ang2);
+			var pt=[x,y];
+			ptscol.push(pt);
+			ang=ang+45;
+		}while (ang<360);
+	}
+	function speed(id,vx,vy){
+		this.velx=vx;
+		this.vely=vy;
+	}
+	function setVx(vx){
+		this.velx=vx;
+	}
+	function setVy(vy){
+		this.vely=vy;
+	}
+	function gethb(){
+		return ptscol;
+	}
+	function isHit(chb){
+		hb2=chb.gethb();
+		for(x=0;x<ptscol.length;x++){
+			pt1=ptscol[x];
+			for(y=0;x<hb2.length;y++){
+				pt2=hb2[y];
+				if(pt1[0]==pt2[0] & pt1[1]==pt2[1]){
+					return true;
+				}
+			}
+			return false;
+		}
 	}
 }
 class playerCircle extends circle{
@@ -26,5 +66,19 @@ class randomCircle extends circle{
 		this.velx=vx;
 		this.vely=vy;
 	}
+	function setX(vx){
+		this.velx=vx;
+	}
+	function setY(vy){
+		this.vely=vy;
+	}
 }
-function getX
+function getX(rd,ang){
+	return rd*Math.cos(ang);
+}
+function getY(rd,ang){
+	return rd.Math.sin(ang);
+}
+function tradAng(deg){
+    return deg * Math.PI / 180;
+};
