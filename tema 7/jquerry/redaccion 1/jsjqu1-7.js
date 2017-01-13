c5cfd4c25e0d20aa3fbb73340534618e6832d6cc
isidro lucena santiago <darksirius91@gmail.com>
2017-01-13 10:18:35 +0100

    45

--- tema 7/jquerry/redaccion 1/jsjqu1-7.js
+++ tema 7/jquerry/redaccion 1/jsjqu1-7.js
@@ -1,3 +1,24 @@
-document.ready(){
-	alert("listo");
+function btnalert1(){
+	alert($(document.head).text());
 }
+function btnalert2(){
+	alert($(document.body).text());
+}
+function palert(){
+	alert($(this).text());
+}
+function phide(){
+	$(this).hide();
+}
+function cambianombre(){
+	$(this).text()
+	$('li').text("#"+$(this).text());
+}
+$(document).ready(function(){
+   alert("carga completa");
+});
+$('#btncb').click(btnalert1);
+$('#btncp').click(btnalert2);
+$('.intro').click(phide);
+$('p :not(.intro)').click(palert);
+cambianombre();
