bb8fc6224b534bdf95828a69b9ec6e325b903519
isidro lucena santiago <darksirius91@gmail.com>
2017-01-16 14:41:10 +0100

    redaccion 1 tema 7 ejercicio 7

--- tema 7/jquerry/redaccion 1/jsjqu1-7.js
+++ tema 7/jquerry/redaccion 1/jsjqu1-7.js
@@ -10,15 +10,24 @@ function palert(){
 function phide(){
 	$(this).hide();
 }
-function cambianombre(){
-	$(this).text()
-	$('li').text("#"+$(this).text());
+function cambianombreli(){
+	$("li").prepend("#");
+}
+function cambianombreEnl(){
+	$("a").text("troll");
 }
 $(document).ready(function(){
    alert("carga completa");
 });
+function cambiacolor(){
+	$(this).css("backgroundColor","#8A0808");
+}
 $('#btncb').click(btnalert1);
 $('#btncp').click(btnalert2);
 $('.intro').click(phide);
 $('p :not(.intro)').click(palert);
-cambianombre();
+cambianombreli();
+$("li").click(phide);
+cambianombreEnl();
+$(".tb1 td").click(cambiacolor);
+$(".tb2 tr").click(phide);
