723eaf9fc546734b4cfb67eb7045a95cb8492f7e
isidro lucena santiago <darksirius91@gmail.com>
2017-01-19 14:04:17 +0100

    ji

---
+++ tema 7/jquerry/redaccion 3/jsjq3-anim.js
@@ -0,0 +1,15 @@
+function ef1(){
+	
+}
+function ef2(){
+	
+}
+function ef3(){
+	
+}
+function ef4(){
+	
+}
+function ena(){
+	$("#d1").
+}
--- tema 7/jquerry/redaccion 3/jsjq3.js
+++ tema 7/jquerry/redaccion 3/jsjq3.js
@@ -1,3 +1,4 @@
+n=2;
 function colortb(){
 	$(".tb tr:lt(2) td").css("backgroundColor","#FF0000");
 	$(".tb tr:gt(2) td").css("backgroundColor","#2E64FE");
@@ -8,6 +9,29 @@ function colorp1(){
 function colortb2(){
 	$(".tb tr td:empty").css("backgroundColor","#FFFF00");
 }
+function drfoc(){
+	$("textarea").focus();
+}
+function colorp2(){
+	$("div p").css("color","#2E64FE");
+}
+function aumft(){
+	if(n==0){
+		$(this).css("font-size","15px");
+	}else{if(n==2){
+			$(this).css("font-size","20px");
+		}else{
+			$(this).css("font-size","35px");
+		};
+	}
+	n--;
+}
 colortb();
 colorp1();
 //colortb2();
+$("#btn1").click(drfoc);
+//$("div p").click(colorp2);
+$("#cre").click(aumft);
+
+
+
