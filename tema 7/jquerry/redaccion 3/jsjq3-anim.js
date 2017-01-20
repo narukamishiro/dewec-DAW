f0d1cdf356ff413829c75afca5ffc74824deaefe
isidro lucena santiago <darksirius91@gmail.com>
2017-01-20 10:22:51 +0100

    lol

--- tema 7/jquerry/redaccion 3/jsjq3-anim.js
+++ tema 7/jquerry/redaccion 3/jsjq3-anim.js
@@ -11,5 +11,23 @@ function ef4(){
 	
 }
 function ena(){
-	$("#d1").
+	$("#d1").fadeIn("slow");
+	$("#d2").fadeOut("slow");
+	$("#d3").fadeOut("fast");
+	$("#d4").fadeToggle( "slow", "linear" );
 }
+function boir(){
+	$("div").clearqueue();
+	$("div").stop();
+}
+function fadeint(){
+	$("#d1").fadeIn("slow");
+	$("#d2").fadeIn("fast");
+	$("#d3").fadeIn(800);
+	$("#d4").fadeIn(2000);
+	$("#d5").fadeIn(4000);
+}
+//$("#btn1").click(ena);
+$("div").hide();
+$("#btn1").click(fadeint);
+$("#btn2").click(boir);
